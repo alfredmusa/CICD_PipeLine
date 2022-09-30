f00289086eaf550b8326447cf9f74d043a31f594
freddiemac10 <109995229+alfredmusa@users.noreply.github.com>
2022-09-30 16:19:17 -0500

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,11 @@
 from utils import add
+import pytest
 
-def test_add():
-    assert add(10,10) == 20
+@pytest.mark.parametrize('x, y, result', [
+    (10, 10, 20),
+    (5, 5, 10),
+    (6, 6, 12)
+])
+def test_add(x, y, result):
+    assert add(x, y) == result
 
