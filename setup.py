3b72fd51800245bf7c5ccb54ead229548354b3b8
freddiemac10 <109995229+alfredmusa@users.noreply.github.com>
2022-10-03 16:49:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 import pathlib
-from Cython.Build import cythonize
+
 setup(
     name='CICD_PipeLine',
     version='7.1.3',
     python_requires="==3.8",
     install_requires=[
-        'pytest','labels','pandas','numpy<1.22.0'
+        "Cython","pytest","labels","pandas","numpy<1.22.0"
     ],
-    packages=find_packages(where="src"),
-    ext_modules=cythonize(ext_modules)
+    packages=find_packages(where="src")
 )
