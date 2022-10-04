ca90dbab868c868a0edd9c1c04dfa13efc1e2b53
freddiemac10 <109995229+alfredmusa@users.noreply.github.com>
2022-10-04 14:07:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     name='CICD_PipeLine',
     version='7.1.3',
     python_requires=">=3.7",
+    setup_requires=['cython', 'numpy'],
     install_requires=[
-        'cython','pytest','labels','pandas','numpy<1.20.0; python_version<"3.7"
+        'cython','pytest','labels','pandas','numpy<1.20.0; python_version<"3.7"'
     ],
     packages=find_packages(where="src")
 )
