a0ec7283f4c9651b627da15fe0f20c1317467b38
freddiemac10 <109995229+alfredmusa@users.noreply.github.com>
2022-10-04 17:10:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+import pathlib
+import os
+import sys
 
 
 setup(
-    name='awesomecode',
-    version='1.0.0',
+    name='CICD_PipeLine',
+    version='7.1.3',
+    python_requires=">=3.7",
+    packages=find_packages(where="src"),
     install_requires=[
         'pytest'
-    ],
-    packages=find_packages()
+    ]
+    
 )
