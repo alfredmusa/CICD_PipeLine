89b0b2dd6599f71dedfe2534d6673f85177095aa
freddiemac10 <109995229+alfredmusa@users.noreply.github.com>
2022-10-04 16:39:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='CICD_PipeLine',
     version='7.1.3',
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     packages=find_packages(where="src"),
     install_requires=[
         'numpy>=1.19.5',
