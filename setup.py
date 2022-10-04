c99a5e18c0dea971ae5fc690a356753634c661c7
freddiemac10 <109995229+alfredmusa@users.noreply.github.com>
2022-10-04 14:10:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     python_requires=">=3.7",
     setup_requires=['cython', 'numpy'],
     install_requires=[
-        'cython','pytest','labels','pandas','numpy<1.20.0; python_version<"3.7"'
+        'cython','pytest','labels','pandas','numpy<1.20.0'
     ],
     packages=find_packages(where="src")
 )
